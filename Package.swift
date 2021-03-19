b2fa9c44a853b94d914a4bc7c664ec41904b18a7
Saravanan Selvam <ssaravanan@zohocorp.com>
2021-03-19 22:23:01 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,6 +13,6 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Apptics",
-            url: "https://github.com/zoho/Apptics/releases/download/\(version)/Apptics.zip", checksum: "55c5f7dd10187b090056f4dc587c3d902587a5513148cd8454d432d5d768079b")
+            url: "https://github.com/zoho/Apptics/releases/download/\(version)/Apptics.zip", checksum: "adf254c014ca10d1887b6c03b75a8fa27eb29c13fd5be7064648d52d753dea58")
     ]
 )
